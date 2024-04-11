169425df551c911127c223a88aaf328ffafada5d
logan_carlson <lacarlson5@gmail.com>
2024-04-10 23:18:34 -0400

    Switch arrow statement

--- hello-world/hello-world/src/Flight.java
+++ hello-world/hello-world/src/Flight.java
@@ -1,5 +1,10 @@
 public class Flight {
     public Flight(int number){
         //Flight Constructor
+
+        switch (number) {
+            case 1 -> System.out.println("One");
+                
+        }
     }
 }
